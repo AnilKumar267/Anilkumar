e77843bf4669da818919e7d81a973b7039f81da5
salomihellary <salomihellary@e48822ac-ed80-4a60-87b4-70136b816a79>
2017-05-26 04:14:39 +0000

    edited

--- trunk/servletBasic/src/main/java/com/slokam/SignPageServlet.java
+++ trunk/servletBasic/src/main/java/com/slokam/SignPageServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 public class SignPageServlet extends GenericServlet{
-
+//ones
 	@Override
 	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
 		//arg0.setAttribute("enter", "u r register data success");
