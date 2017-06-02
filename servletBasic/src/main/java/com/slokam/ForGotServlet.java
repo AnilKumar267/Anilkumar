ce3a7daef282b1e909cd4b249dedc567baf63688
bhargavi <bhargavi@e48822ac-ed80-4a60-87b4-70136b816a79>
2017-06-02 13:57:02 +0000

    commit for forgotservlet

--- servletBasic/src/main/java/com/slokam/ForGotServlet.java
+++ servletBasic/src/main/java/com/slokam/ForGotServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ForGotServlet extends HttpServlet{
-
+//hello this is abc
 	//override
 	//Hi THis is bharath
 	@Override
